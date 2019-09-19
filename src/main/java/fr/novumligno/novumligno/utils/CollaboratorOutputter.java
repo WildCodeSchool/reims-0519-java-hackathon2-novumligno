ea8ff95521bfd6ba992a871b2118a2b5db9a4780
Dupuis Sebastien <sebastien-dupuis@laposte.net>
2019-09-19 21:56:42 +0200

    add all Collaborator

--- src/main/java/fr/novumligno/novumligno/utils/CollaboratorOutputter.java
+++ src/main/java/fr/novumligno/novumligno/utils/CollaboratorOutputter.java
@@ -76,141 +76,142 @@ public class CollaboratorOutputter implements CommandLineRunner {
                 "William Huet",
                 "Responsable Service Agronomique",
                 "Agronomie",
-                "",
-                "",
-                ""
+                "Innovation",
+                "https://www.linkedin.com/in/huet-william-31051286/",
+                "https://media.licdn.com/dms/image/C4E03AQFEiOABhkGpFQ/profile-displayphoto-shrink_200_200/0?e=1574294400&v=beta&t=UCVVdLM_GUgMllWLXz39O0bAz-HRujUXfZJugr5-XzA"
             },
 
             {
                 "Jean-Baptiste Vervy",
                 "CEO Ter'Informatique & Directeur de la stratégie digitale Groupe FDSEA51 ",
                 "Numérique",
-                "",
-                "",
-                ""
+                "Management",
+                "https://www.linkedin.com/in/jean-baptiste-vervy-a2202999/",
+                "https://media.licdn.com/dms/image/C4E03AQEmcRrQYQK58A/profile-displayphoto-shrink_200_200/0?e=1574294400&v=beta&t=eKyE94m0P2c8_fZamGkqzq3zBbMQiaqypdVlIgruHLI"
             },
 
             {
                 "Jean-Marie Lett",
                 "Responsable du service Conseil",
                 "Economie",
+                "Machinisme",
                 "",
-                "",
-                ""
+                "https://www.iasquebec.com/wp-content/themes/ATYPIC/public/img/profile-default-male.png"
             },
 
             {
                 "Carole Leverrier",
                 "Responsable Innovation Bioressources",
                 "Innovation",
-                "",
-                "",
-                ""
+                "Valorisation",
+                "https://www.linkedin.com/in/carole-leverrier-bb69b7111/",
+                "https://us.123rf.com/450wm/kritchanut/kritchanut1406/kritchanut140600106/29213216-stock-vector-female-avatar-silhouette-profile-pictures.jpg?ver=6",
             },
 
             {
                 "Emmanuel Foissy",
                 "Responsable Marketing",
                 "Marketing",
+                "Economie",
                 "",
-                "",
-                ""
+                "https://www.iasquebec.com/wp-content/themes/ATYPIC/public/img/profile-default-male.png"
             },
             {
                 "Mélanie Franche",
                 "Ingénieur Régional Champagne-Ardenne",
                 "Agronomie",
+                "Biologie",
                 "",
-                "",
-                ""
+                "https://us.123rf.com/450wm/kritchanut/kritchanut1406/kritchanut140600106/29213216-stock-vector-female-avatar-silhouette-profile-pictures.jpg?ver=6"
             },
             {
                 "Laure Metzger",
                 "Directrice",
                 "Pédologie",
-                "",
-                "",
-                ""
+                "Biologie",
+                "https://www.linkedin.com/in/laure-metzger-0861504/",
+                "https://us.123rf.com/450wm/kritchanut/kritchanut1406/kritchanut140600106/29213216-stock-vector-female-avatar-silhouette-profile-pictures.jpg?ver=6",
             },
             {
                 "Sébastien Debuisson",
                 "Chef du service Vigne",
                 "Agronomie",
+                "Management",
                 "",
-                "",
-                ""
+                "https://www.iasquebec.com/wp-content/themes/ATYPIC/public/img/profile-default-male.png"
             },
             {
                 "Sébastien Pinet",
                 "Directeur",
                 "Numérique",
-                "",
-                "",
-                ""
+                "Zootechnie",
+                "https://www.linkedin.com/in/sebastien-pinet-724456158/",
+                "https://www.iasquebec.com/wp-content/themes/ATYPIC/public/img/profile-default-male.png",
             },
             {
                 "Alexandre Laflotte",
                 "Directeur Domaine expérimental de la Bouzule",
                 "Agronomie",
+                "Zootechnie",
                 "",
-                "",
-                ""
+                "https://www.iasquebec.com/wp-content/themes/ATYPIC/public/img/profile-default-male.png"
+                
             },
             {
                 "Alexandre Ducruet",
                 "Directeur territorial",
                 "Valorisation",
-                "",
-                "",
-                ""
+                "Management",
+                "https://www.linkedin.com/in/ducruet-alexandre-1298b08/",
+                "https://media.licdn.com/dms/image/C4E03AQFqb4DJaFzunw/profile-displayphoto-shrink_200_200/0?e=1574294400&v=beta&t=Jv7gkHA2qhrpiFtlZcdWdLpfEsgZQ5T_qGiWs6JfLFA"
             },
             {
                 "Olivier Descroizette",
                 "Directeur technique Innovation chez bioline by InVivo",
                 "Innovation",
-                "",
-                "",
-                ""
+                "Numérique",
+                "https://www.linkedin.com/in/olivier-descroizette-399821a1/",
+                "https://media.licdn.com/dms/image/C4D03AQFwlOK78LhQRA/profile-displayphoto-shrink_200_200/0?e=1574294400&v=beta&t=No5B-OW0wMe5ldKireajgp373hSPGJO3a5U8Vloh3Cw",
             },
             {
                 "Eric Perrin",
                 "Maître de Conférences & Co-animateur de l'axe 'smart agriculture's",
                 "Numérique",
-                "",
-                "",
-                ""
+                "Innovation",
+                "https://www.researchgate.net/profile/E_Perrin",
+                "https://www.iasquebec.com/wp-content/themes/ATYPIC/public/img/profile-default-male.png"
             },
             {
                 "Arnaud Gilotin",
                 "Président & Pilote-Instructeur-Concepteur",
                 "Machinisme",
-                "",
-                "",
-                ""
+                "Numérique",
+                "https://www.linkedin.com/in/arnaud-gilotin-05827b156/",
+                "https://media.licdn.com/dms/image/C4D03AQF8UGmP_-iAZA/profile-displayphoto-shrink_200_200/0?e=1574294400&v=beta&t=7QDP5BQau4LOuGeozBHSlh0oQ5w6Uqfjw0ozjxiiOYc"
             },
             {
                 "Mathias Sexe",
                 "Directeur Agronomie et Développement",
                 "Agronomie",
-                "",
-                "",
-                ""
+                "Innovation",
+                "https://www.linkedin.com/in/mathias-sexe-81317978/?originalSubdomain=fr",
+                "https://media.licdn.com/dms/image/C4E03AQEDOMgw6aEZLg/profile-displayphoto-shrink_200_200/0?e=1574294400&v=beta&t=2MAOqIR8fuyhZbIRXbANEcZbuWxto1k6saQqJIW1lCU"
             },
             {
                 "Maxime Luneau",
                 "Ingénieur Marketing Développement Commercial",
                 "Protection des cultures",
-                "",
-                "",
-                ""
+                "Marketing",
+                "http://fr.viadeo.com/fr/profile/maxime.luneau4",
+                "http://static8.viadeo-static.com/QBhAivkS4EtBCC7SXIl9Y-boXCw=/300x300/member/0021djgjq6szzvm5%3Fts%3D1386671078000"
             },
             {
                 "Renaud Font",
                 "Responsable Commercial",
                 "Numérique",
-                "",
-                "",
-                ""
+                "Marketing",
+                "https://www.linkedin.com/in/renaud-font/",
+                "https://media.licdn.com/dms/image/C5603AQGTM4GoRx_MKg/profile-displayphoto-shrink_200_200/0?e=1574294400&v=beta&t=Tbt1LWma87R86sR09ol7TiuYP85YEAhIogJ8LFhiIW4" 
             },
 
         };
