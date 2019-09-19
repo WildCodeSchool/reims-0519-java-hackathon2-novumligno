b962d5dc425b7342ff7c202173ce4ff1a64a83e5
Dupuis Sebastien <sebastien-dupuis@laposte.net>
2019-09-19 14:15:46 +0200

    add entity Company

---
+++ src/main/java/fr/novumligno/novumligno/entities/Company.java
@@ -0,0 +1,121 @@
+package fr.novumligno.novumligno.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Company {
+
+    public Company() {
+
+    }
+
+    public Company(String name, String logo, String mission, String address, String website){
+        this.name = name;
+        this.logo = logo;
+        this.mission = mission;
+        this.address = address;
+        this.website = website;
+
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+    private String name;
+    private String logo;
+    private String mission;
+    private String address;
+    private String website;
+    
+
+
+    /**
+     * @return Long return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return String return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return String return the logo
+     */
+    public String getLogo() {
+        return logo;
+    }
+
+    /**
+     * @param logo the logo to set
+     */
+    public void setLogo(String logo) {
+        this.logo = logo;
+    }
+
+    /**
+     * @return String return the mission
+     */
+    public String getMission() {
+        return mission;
+    }
+
+    /**
+     * @param mission the mission to set
+     */
+    public void setMission(String mission) {
+        this.mission = mission;
+    }
+
+    /**
+     * @return String return the address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * @param address the address to set
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     * @return String return the website
+     */
+    public String getWebsite() {
+        return website;
+    }
+
+    /**
+     * @param website the website to set
+     */
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+}
