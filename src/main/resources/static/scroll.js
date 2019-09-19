3a93889bdee0ee23fda625c5b6d65252ba802777
Lea Goarant <leagoarant@msn.com>
2019-09-19 23:42:08 +0200

    ajout du js

--- src/main/resources/static/scroll.js
+++ src/main/resources/static/scroll.js
@@ -1,17 +1,8 @@
+console.log("hello global")
 $(function() {
-    /**
-    * Smooth scrolling to page anchor on click
-    **/
-    $("a[href*='#']:not([href='#'])").click(function() {
-        if (
-            location.hostname == this.hostname
-            && this.pathname.replace(/^\//,"") == location.pathname.replace(/^\//,"")
-        ) {
-            var anchor = $(this.hash);
-            anchor = anchor.length ? anchor : $("[name=" + this.hash.slice(1) +"]");
-            if ( anchor.length ) {
-                $("html, body").animate( { scrollTop: anchor.offset().top }, 1500);
-            }
-        }
+    console.log("hello")
+    $('a[href*=#]').on('click', function(e) {
+      e.preventDefault();
+      $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
     });
-});
+  });
